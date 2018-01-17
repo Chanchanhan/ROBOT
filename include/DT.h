1551e44ad0ce09806811d97edc020331366376d6
zaihao717 <qqh717@163.com>
2018-01-17 17:04:55 +0800

    to push

--- include/DT.h
+++ include/DT.h
@@ -10,7 +10,7 @@
 #include <opencv2/opencv.hpp>
 
 // Calculates the distance trasnform as described in Distance Transforms of Sampled Functions
-void distanceTransfqorm(const cv::Mat &inputMatrix, cv::Mat &outputMatrix,
+void distanceTransform(const cv::Mat &inputMatrix, cv::Mat &outputMatrix,
                        cv::Mat &locations,std::vector<float> weights = std::vector<float>());
 
 
